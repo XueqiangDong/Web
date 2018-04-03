1e3cc676c76e1b8a2434bb92c93b281a745a68b1
Xueqiang Dong <dxq7541@gmail.com>
2018-04-02 23:28:16 -0400

    jQuery effect 02 after check

--- js/click.js
+++ js/click.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $("#b1").click(function(){
-        $("#p1").hide(1000);
+        $("#p1").hide();
     });
     $("#b2").click(function(){
         $("#p2").hide();
@@ -8,24 +8,35 @@ $(document).ready(function(){
     $("button#b3").click(function(){
         $(this).hide();
     });
-    $("p").css("color","green");
+
     $("button#b4").click(function(){
-        $("p").show(1000,changeColor());
-        $("button").show();
+        $("#p1,#p2,#b3").show(1000,changeColor());
     });
     $("button#b5").click(function(){
-        $("#div1").fadeToggle();
-        $("#div2").fadeToggle(1000);
-        $("#div3").fadeToggle(3000);
+        $("#div1p1").fadeToggle();
+        $("#div1p2").fadeToggle(1000);
+        $("#div1p3").fadeToggle(3000);
     });
     $("button#b6").click(function(){
-        $("#div1").fadeTo(100,0.2);
-        $("#div2").fadeTo(100,0.2);
-        $("#div3").fadeTo(100,0.2);
+        $("#div1p1").fadeTo(100,0.2);
+        $("#div1p2").fadeTo(100,0.2);
+        $("#div1p3").fadeTo(100,0.2);
+    });
+    $(".flip").click(function () {
+        $(".panel").slideToggle("slow");
+    });
+    $("button#b7").click(function(){
+        var div2 = $("#div2");
+        div2.animate({marginLeft:'+=10px', marginTop:'+=5px',/*opacity:'0.3',*/ height:'100px',width:'+=10px'},"slow");
+        div2.animate({marginLeft:'+=10px', marginTop:'+=5px',/*opacity:'0.3',*/ height:'200px',width:'+=0px'},"slow");
+        div2.animate({marginLeft:'+=10px', marginTop:'+=5px',/*opacity:'0.3',*/ height:'200px',width:'+=100px'},"slow");
+        div2.animate({marginLeft:'+=10px', marginTop:'+=5px',/*opacity:'0.3',*/ height:'100px',width:'-=110px'},"slow");
+        div2.animate({marginLeft:'+=0px', marginTop:'+=0px',/*opacity:'0.3',*/ height:'toggle',width:'-=0px'},"slow");
+        div2.animate({marginLeft:'+=0px', marginTop:'+=0px',/*opacity:'0.3',*/ height:'toggle',width:'-=0px'},"slow");
     });
 });
 function changeColor() {
-	$("p").css("color","red");
+	$("#p1,#p2,#b3").css("color","red");
 }
 
 /* 如果$被其他占用，可以改名
